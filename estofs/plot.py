e05604b87174128df9ff50b571bb42ded0c24a01
Sergey Vinogradov <grapesh@gmail.com>
2018-03-04 18:29:02 -0500

    Update plot.py

--- estofs/plot.py
+++ estofs/plot.py
@@ -60,16 +60,13 @@ def setStationPlot (master, stationName, cwl, n, oldxlim, ylim):
     fl_minor_ft   = np.nan
     fl_moder_ft   = np.nan
     fl_major_ft   = np.nan
-    obs = dict()
     new_xlim0 = oldxlim[0]
-    obs['dates']  = []
-    obs['values'] = []
+    
     stationTitle  = stationName    
     # Get data from master list
     for m in master:
         nosid = m[query.index('NOSID')]
         if nosid in stationName:
-            #print '[info]: station identified: ' + nosid
             try:
                 stationTitle  = m[query.index('NOSID')] + ' ' + m[query.index('Name')]
                 datum_hat_ft  = float(m[query.index('ETSS HAT-ft')])
@@ -101,67 +98,60 @@ def setStationPlot (master, stationName, cwl, n, oldxlim, ylim):
     ax2 = ax.twinx()
     ax.plot([],[])
 
-    if not np.isnan(fl_minor_ft*fl_moder_ft*fl_major_ft*datum_mhhw_ft*datum_msl_ft):
-    
-        fl_minor_m = 1./3.28084*(datum_mhhw_ft+fl_minor_ft-datum_msl_ft) 
-        if fl_minor_m < ylim[1]:
-            ax.plot(xlim[0], fl_minor_m, 'dr', markerfacecolor='r')
-            ax.text(xlim[0], fl_minor_m + 0.*cwl['zeta'][2,n],\
-                    'Minor Flood: ' + str(np.round(fl_minor_m,2)),color='k',fontsize=7)
-            p = patches.Rectangle((mdates.date2num(xlim[0]), fl_minor_m), \
-                                  mdates.date2num(xlim[1])-mdates.date2num(xlim[0]), \
-                                  ylim[1]-fl_minor_m, \
-                                  color='r',alpha=0.15)
-            ax.add_patch(p)
-                
-        fl_moder_m = 1./3.28084*(datum_mhhw_ft+fl_moder_ft-datum_msl_ft) 
-        if fl_moder_m < ylim[1]:
-            ax.plot(xlim[0], fl_moder_m, 'dr', markerfacecolor='r')
-            ax.text(xlim[0], fl_moder_m + 0.*cwl['zeta'][2,n],\
-                    'Moderate Flood: '+ str(np.round(fl_moder_m,2)),color='k',fontsize=7)
-            p = patches.Rectangle((mdates.date2num(xlim[0]), fl_moder_m), \
-                                  mdates.date2num(xlim[1])-mdates.date2num(xlim[0]), \
-                                  ylim[1]-fl_moder_m, \
-                                  color='r',alpha=0.15)
-            ax.add_patch(p)
+    fl_minor_m = 1./3.28084*(datum_mhhw_ft+fl_minor_ft-datum_msl_ft) 
+    if not np.isnan(fl_minor_m) and fl_minor_m < ylim[1]:
+        ax.plot(xlim[0], fl_minor_m, 'dr', markerfacecolor='r')
+        ax.text(xlim[0], fl_minor_m,\
+                'Minor Flood: ' + str(np.round(fl_minor_m,2)),color='k',fontsize=7)
+        p = patches.Rectangle((mdates.date2num(xlim[0]), fl_minor_m), \
+                              mdates.date2num(xlim[1])-mdates.date2num(xlim[0]), \
+                              ylim[1]-fl_minor_m, \
+                              color='r',alpha=0.15)
+        ax.add_patch(p)
+            
+    fl_moder_m = 1./3.28084*(datum_mhhw_ft+fl_moder_ft-datum_msl_ft) 
+    if not np.isnan(fl_moder_m) and fl_moder_m < ylim[1]:
+        ax.plot(xlim[0], fl_moder_m, 'dr', markerfacecolor='r')
+        ax.text(xlim[0], fl_moder_m,\
+                'Moderate Flood: '+ str(np.round(fl_moder_m,2)),color='k',fontsize=7)
+        p = patches.Rectangle((mdates.date2num(xlim[0]), fl_moder_m), \
+                              mdates.date2num(xlim[1])-mdates.date2num(xlim[0]), \
+                              ylim[1]-fl_moder_m, \
+                              color='r',alpha=0.15)
+        ax.add_patch(p)
 
-        fl_major_m = 1./3.28084*(datum_mhhw_ft+fl_major_ft-datum_msl_ft) 
-        if fl_major_m < ylim[1]:
-            ax.plot(xlim[0], fl_major_m, 'dr', markerfacecolor='r')
-            ax.text(xlim[0], fl_major_m + 0.*cwl['zeta'][2,n],\
-                    'Major Flood: ' + str(np.round(fl_major_m,2)),color='k',fontsize=7)
-            p = patches.Rectangle((mdates.date2num(xlim[0]), fl_major_m), \
-                                  mdates.date2num(xlim[1])-mdates.date2num(xlim[0]), \
-                                  ylim[1]-fl_major_m, \
-                                  color='r',alpha=0.15)
-            ax.add_patch(p)
+    fl_major_m = 1./3.28084*(datum_mhhw_ft+fl_major_ft-datum_msl_ft) 
+    if not np.isnan(fl_major_m) and fl_major_m < ylim[1]:
+        ax.plot(xlim[0], fl_major_m, 'dr', markerfacecolor='r')
+        ax.text(xlim[0], fl_major_m,\
+                'Major Flood: ' + str(np.round(fl_major_m,2)),color='k',fontsize=7)
+        p = patches.Rectangle((mdates.date2num(xlim[0]), fl_major_m), \
+                              mdates.date2num(xlim[1])-mdates.date2num(xlim[0]), \
+                              ylim[1]-fl_major_m, \
+                              color='r',alpha=0.15)
+        ax.add_patch(p)
 
-    if datum_msl_ft is not np.nan:
+    datum_mhhw_m = 1./3.28084*(datum_mhhw_ft-datum_msl_ft) 
+    if not np.isnan(datum_mhhw_m) and datum_mhhw_m < ylim[1]:
+        ax.plot(xlim, [datum_mhhw_m, datum_mhhw_m], color='c')
+        ax.plot(xlim[1], datum_mhhw_m, 'dc', markerfacecolor='c')
+        ax.text(cwl['time'][-80], datum_mhhw_m + 0.05, 'MHHW',color='c',fontsize=7)
 
-        datum_mhhw_m = 1./3.28084*(datum_mhhw_ft-datum_msl_ft) 
-        if datum_mhhw_m < ylim[1]:
-            ax.plot(xlim, [datum_mhhw_m, datum_mhhw_m], color='c')
-            ax.plot(xlim[1], datum_mhhw_m, 'dc', markerfacecolor='c')
-            ax.text(cwl['time'][-60], datum_mhhw_m + 0.*cwl['zeta'][2,n] +0.05, 'MHHW',color='c')
+    datum_mllw_m = 1./3.28084*(datum_mllw_ft-datum_msl_ft) 
+    if not np.isnan(datum_mllw_m) and datum_mllw_m > ylim[0] and datum_mllw_m < ylim[1]:
+        ax.plot(xlim, [datum_mllw_m, datum_mllw_m], color='c')
+        ax.plot(xlim[1], datum_mllw_m, 'dc', markerfacecolor='c')
+        ax.text(cwl['time'][-80], datum_mllw_m + 0.05, 'MLLW',color='c',fontsize=7)
 
-            datum_mllw_m = 1./3.28084*(datum_mllw_ft-datum_msl_ft) 
-            if datum_mllw_m > ylim[0]:
-                ax.plot(xlim, [datum_mllw_m, datum_mllw_m], color='c')
-                ax.plot(xlim[1], datum_mllw_m, 'dc', markerfacecolor='c')
-                ax.text(cwl['time'][-60], datum_mllw_m + 0.*cwl['zeta'][2,n] +0.05, 'MLLW',color='c')
-
-            datum_hat_m  = 1./3.28084*(datum_hat_ft-datum_msl_ft) 
-            if datum_hat_m < ylim[1]:
-                ax.plot(xlim, [datum_hat_m, datum_hat_m], color='y')
-                ax.plot(xlim[1], datum_hat_m, 'dy', markerfacecolor='y')
-                ax.text(cwl['time'][-60], datum_hat_m + 0.*cwl['zeta'][2,n] +0.05, 'HAT',color='y')
+    datum_hat_m  = 1./3.28084*(datum_hat_ft-datum_msl_ft) 
+    if not np.isnan(datum_hat_m) and datum_hat_m < ylim[1]:
+        ax.plot(xlim, [datum_hat_m, datum_hat_m], color='y')
+        ax.plot(xlim[1], datum_hat_m, 'dy', markerfacecolor='y')
+        ax.text(cwl['time'][-60], datum_hat_m  + 0.05, 'HAT',color='y',fontsize=7)
 
     ax.plot(xlim[1], 0, 'dk',color='k')
-    ax.text(cwl['time'][-60], 0.*cwl['zeta'][2,n] +0.05, 'LMSL',color='k')
-        
-    ax.set_xlabel('DATE UTC')
-    ax.grid(True,which='both')   
-        
+    ax.text(cwl['time'][-120], 0.05, 'LMSL',color='k',fontsize=7)
+            
     try:
         ax.plot(obs['dates'], obs['values'],        
                 color='g',label='OBSERVED',  linewidth=2.0)
@@ -173,6 +163,10 @@ def setStationPlot (master, stationName, cwl, n, oldxlim, ylim):
                 str(np.round(peak_obs_val,1)) + "m (" + 
                    str(np.round(3.28084*peak_obs_val,1)) +"ft)", color='g',
                    fontsize=7, weight='bold')
+        
+        ax.plot( [now, now], ylim, 'k',linewidth=1)
+        ax.text(  now,  ylim[1]-0.2,'NOW', color='k',fontsize=6)
+        
     except:
         pass     
         
@@ -191,6 +185,7 @@ def stations (cwlFile, htpFile, pp, titleStr, plotPath, args):
     masterListRemote = pp['Stations']['url']
     masterListLocal  = 'masterlist.csv'
     csdlpy.transfer.download(masterListRemote, masterListLocal)
+    
     query = ['NOSID','Name',\
              'ETSS HAT-ft','ETSS MSL-ft','ETSS MLLW-ft','ETSS MHHW-ft', \
              'Minor MHHW ft','Moderate MHHW ft','Major MHHW ft']
@@ -209,11 +204,12 @@ def stations (cwlFile, htpFile, pp, titleStr, plotPath, args):
     for n in range(nStations):
         fig, ax, ax2, xlim, stationTitle = \
                 setStationPlot (master, cwl['stations'][n], cwl, n, xlim, ylim)
-
+        
         plt.title(titleStr + ' :: ' + stationTitle, fontsize=10)
 
         peak_val = np.nanmax(cwl['zeta'][:,n])
         peak_dat = cwl['time'][np.argmax(cwl['zeta'][:,n])]
+               
         ax.plot(peak_dat, peak_val, 'bo')
         ax.text(peak_dat, 1.05*peak_val, 
                 str(np.round(peak_val,1)) + "m (" + 
@@ -223,7 +219,7 @@ def stations (cwlFile, htpFile, pp, titleStr, plotPath, args):
         peak_str = str(peak_dat.hour).zfill(2) + ':' + str(peak_dat.minute).zfill(2)
         ax.text(peak_dat, ylim[0], peak_str ,color='b')            
 
-        ax.text(xlim[0],ylim[1]+0.2,'NOAA/NOS')
+        ax.text(xlim[0],ylim[1]+0.1,'NOAA/NOS')
 
         ax.plot(cwl['time'], 0.*cwl['zeta'][:,n],   color='gray')
         ax.plot(htp['time'], htp['zeta'][:,n],      color='c',label='ASTRON TIDE')
@@ -235,6 +231,9 @@ def stations (cwlFile, htpFile, pp, titleStr, plotPath, args):
 
         ax.set_ylabel ('WATER LEVELS, meters MSL')
         ax2.set_ylabel('WATER LEVELS, feet MSL')
+        ax.set_xlabel('DATE UTC')
+        ax.grid(True,which='both')   
+
         ax.set_xlim (        xlim)
         ax.set_ylim (        ylim)
         ax2.set_ylim(3.28084*ylim[0], 3.28084*ylim[1])
