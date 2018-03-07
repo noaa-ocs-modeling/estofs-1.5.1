7a25aa1315cc46f596241b94ed382104cf42e3c9
Sergey Vinogradov <grapesh@gmail.com>
2018-03-06 21:00:33 -0500

    Update test_pac.bash

--- scripts/test_pac.bash
+++ scripts/test_pac.bash
@@ -8,7 +8,7 @@ export logFile=${platform}"/gpfs/hps3/nos/noscrub/polar/estofs_pac/estofs_post.l
 
 export ofsDir=${platform}"/gpfs/hps/nco/ops/com/estofs/prod/"
 export basin="pac"
-export stormCycle="2018030218"
+export stormCycle="latest" #"2018030218"
 export outputDir=${platform}"/gpfs/hps3/nos/noscrub/polar/estofs_pac/"
 export tmpDir=${platform}"/gpfs/hps3/nos/noscrub/tmp/"
 export pltCfgFile=${platform}"/gpfs/hps3/nos/noscrub/nwprod/estofs-1.5.1/scripts/config.plot.estofs.pac.ini"
