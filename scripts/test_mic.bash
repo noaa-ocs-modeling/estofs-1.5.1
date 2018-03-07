d7bcce65409f9a8b417a6c8de0dcd5c12e8a6eb8
Sergey Vinogradov <grapesh@gmail.com>
2018-03-06 20:59:54 -0500

    Update test_mic.bash

--- scripts/test_mic.bash
+++ scripts/test_mic.bash
@@ -8,7 +8,7 @@ export logFile=${platform}"/gpfs/hps3/nos/noscrub/polar/estofs_mic/estofs_post.l
 
 export ofsDir=${platform}"/gpfs/hps/nco/ops/com/estofs/prod/"
 export basin="mic"
-export stormCycle="2018030218"
+export stormCycle="latest" #"2018030218"
 export outputDir=${platform}"/gpfs/hps3/nos/noscrub/polar/estofs_mic/"
 export tmpDir=${platform}"/gpfs/hps3/nos/noscrub/tmp/"
 export pltCfgFile=${platform}"/gpfs/hps3/nos/noscrub/nwprod/estofs-1.5.1/scripts/config.plot.estofs.mic.ini"
